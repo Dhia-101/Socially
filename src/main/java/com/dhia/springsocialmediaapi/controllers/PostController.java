5440a7d5dd7bf94f4f49795604dc34c2db14e9a3
DhiaTr <trabelsidhia19@gmail.com>
2021-12-22 16:02:36 +0100

    add postService to postController & return posts function

--- src/main/java/com/dhia/springsocialmediaapi/controllers/PostController.java
+++ src/main/java/com/dhia/springsocialmediaapi/controllers/PostController.java
@@ -1,16 +1,28 @@
 package com.dhia.springsocialmediaapi.controllers;
 
+import com.dhia.springsocialmediaapi.model.Post;
+import com.dhia.springsocialmediaapi.services.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostController {
 
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/")
-    public String getAllPosts() {
-        return "all posts";
+    public Set<Post> getAllPosts() {
+        return postService.findAll();
     }
 
 }
