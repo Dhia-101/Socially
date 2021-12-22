8d40db90f4ca527ffe16dd19dcb59c45520c27a3
DhiaTr <trabelsidhia19@gmail.com>
2021-12-22 15:56:50 +0100

    add postJpaImplementation; add Bootstrap DataLoader; load few posts on startup

---
+++ src/main/java/com/dhia/springsocialmediaapi/bootstrap/DataLoader.java
@@ -0,0 +1,29 @@
+package com.dhia.springsocialmediaapi.bootstrap;
+
+import com.dhia.springsocialmediaapi.model.Post;
+import com.dhia.springsocialmediaapi.repositories.PostRepository;
+import com.dhia.springsocialmediaapi.services.PostService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final PostRepository postRepository;
+
+    public DataLoader(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Post post1 = new Post("post1");
+        Post post2 = new Post("post2");
+        Post post3 = new Post("post3");
+
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+
+    }
+}
---
+++ src/main/java/com/dhia/springsocialmediaapi/services/jpaImplementation/PostJpaService.java
@@ -0,0 +1,41 @@
+package com.dhia.springsocialmediaapi.services.jpaImplementation;
+
+import com.dhia.springsocialmediaapi.model.Post;
+import com.dhia.springsocialmediaapi.repositories.PostRepository;
+import com.dhia.springsocialmediaapi.services.PostService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PostJpaService implements PostService {
+
+    private final PostRepository postRepository;
+
+    public PostJpaService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public Set<Post> findAll() {
+        Set<Post> posts = new HashSet<>();
+        postRepository.findAll().forEach(posts::add);
+        return posts;
+    }
+
+    @Override
+    public Post findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
