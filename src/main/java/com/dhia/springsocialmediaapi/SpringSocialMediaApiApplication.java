590b501e5875207349c8f3f631ea9fb9fee77cbb
DhiaTr <trabelsidhia19@gmail.com>
2021-12-22 13:43:48 +0100

    init project

---
+++ src/main/java/com/dhia/springsocialmediaapi/SpringSocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.dhia.springsocialmediaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialMediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhia/springsocialmediaapi/SpringSocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhia.springsocialmediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
