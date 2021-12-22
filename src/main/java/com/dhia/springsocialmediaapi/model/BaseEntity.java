9331d6f8b451e3569d1b16b75ef5790abed7aac0
DhiaTr <trabelsidhia19@gmail.com>
2021-12-22 15:30:43 +0100

    add BaseEntity & Post Classes; add jpa dependency

---
+++ src/main/java/com/dhia/springsocialmediaapi/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.dhia.springsocialmediaapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ src/main/java/com/dhia/springsocialmediaapi/model/Post.java
@@ -0,0 +1,23 @@
+package com.dhia.springsocialmediaapi.model;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "post")
+public class Post extends BaseEntity {
+
+    @Column(name = "content")
+    private String content;
+}
