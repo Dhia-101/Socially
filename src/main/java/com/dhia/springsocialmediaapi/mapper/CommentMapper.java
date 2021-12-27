c29d41592a0f50f4a2818c317467f19ccf36473d
DhiaTr <trabelsidhia19@gmail.com>
2021-12-27 15:31:06 +0100

    add commentMapper class annotation; add CommentJpaService findAll method code

--- src/main/java/com/dhia/springsocialmediaapi/mapper/CommentMapper.java
+++ src/main/java/com/dhia/springsocialmediaapi/mapper/CommentMapper.java
@@ -2,8 +2,10 @@ package com.dhia.springsocialmediaapi.mapper;
 
 import com.dhia.springsocialmediaapi.domain.Comment;
 import com.dhia.springsocialmediaapi.model.CommentDTO;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface CommentMapper {
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
 
--- src/main/java/com/dhia/springsocialmediaapi/services/jpaImplementation/CommentJpaService.java
+++ src/main/java/com/dhia/springsocialmediaapi/services/jpaImplementation/CommentJpaService.java
@@ -1,15 +1,32 @@
 package com.dhia.springsocialmediaapi.services.jpaImplementation;
 
+import com.dhia.springsocialmediaapi.mapper.CommentMapper;
 import com.dhia.springsocialmediaapi.model.CommentDTO;
+import com.dhia.springsocialmediaapi.repositories.CommentRepository;
 import com.dhia.springsocialmediaapi.services.CommentService;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CommentJpaService implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentMapper commentMapper;
+
+    public CommentJpaService(CommentRepository commentRepository, CommentMapper commentMapper) {
+        this.commentRepository = commentRepository;
+        this.commentMapper = commentMapper;
+    }
 
     @Override
     public List<CommentDTO> findAll() {
-        return null;
+        List<CommentDTO> comments = new ArrayList<>();
+        commentRepository.findAll().forEach(comment -> {
+            comments.add(commentMapper.commentToCommentDTO(comment));
+        });
+        return comments;
     }
 
     @Override
