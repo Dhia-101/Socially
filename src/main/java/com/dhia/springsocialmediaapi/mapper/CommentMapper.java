0a478cf218fa92ba7462fd13e181ecd45c0810d1
DhiaTr <trabelsidhia19@gmail.com>
2021-12-27 15:00:18 +0100

    set comment mapper mapper; set CommentJpaService basic structure

---
+++ src/main/java/com/dhia/springsocialmediaapi/mapper/CommentMapper.java
@@ -0,0 +1,13 @@
+package com.dhia.springsocialmediaapi.mapper;
+
+import com.dhia.springsocialmediaapi.domain.Comment;
+import com.dhia.springsocialmediaapi.model.CommentDTO;
+import org.mapstruct.factory.Mappers;
+
+public interface CommentMapper {
+    CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
+
+    CommentDTO commentToCommentDTO(Comment comment);
+
+    Comment CommentDTOToComment(CommentDTO commentDTO);
+}
---
+++ src/main/java/com/dhia/springsocialmediaapi/services/jpaImplementation/CommentJpaService.java
@@ -0,0 +1,34 @@
+package com.dhia.springsocialmediaapi.services.jpaImplementation;
+
+import com.dhia.springsocialmediaapi.model.CommentDTO;
+import com.dhia.springsocialmediaapi.services.CommentService;
+
+import java.util.List;
+
+public class CommentJpaService implements CommentService {
+
+    @Override
+    public List<CommentDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public CommentDTO findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public CommentDTO update(Long aLong, CommentDTO object) {
+        return null;
+    }
+
+    @Override
+    public CommentDTO save(CommentDTO object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+}
